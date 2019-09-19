043d4ddc6c4de9aa5853dc2a3c817f56f8730c1b
charlesb4a <charles@back4app.com>
2019-09-19 20:51:47 -0300

    Update delete.py

--- delete.py
+++ delete.py
@@ -2,9 +2,9 @@ import json
 import http.client
 connection = http.client.HTTPSConnection('parseapi.back4app.com', 443)
 connection.connect()
-connection.request('DELETE', '/classes/Likes/bUZ5Zuav2m', '', {
-       "X-Parse-Application-Id": "lOnJ1j6c5OHzz7RFmlXATbEqGvKA3BuT9KXGyWdO",
-       "X-Parse-REST-API-Key": "sKio73fB6o0cAH7m0CbMaaHZk9GiAjiAQ5nzXYKL"
+connection.request('DELETE', '/classes/<CLASS-NAME>/<OBJECT-ID>', '', {
+       "X-Parse-Application-Id": "<APP-ID-HERE>",
+       "X-Parse-REST-API-Key": "<REST-KEY-HERE>"
      })
 result = json.loads(connection.getresponse().read().decode())
 print (result)
