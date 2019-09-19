dcb6eb3c83fc28f21d4302483526dd664a331211
charlesb4a <charles@back4app.com>
2019-09-19 20:43:16 -0300

    Update update.py

--- update.py
+++ update.py
@@ -2,13 +2,13 @@ import json
 import http.client
 connection = http.client.HTTPSConnection('parseapi.back4app.com', 443)
 connection.connect()
-connection.request('PUT', '/classes/Likes/WrA5D0hZJu', json.dumps({
+connection.request('PUT', '/classes/<CLASS-NAME>/<OBJECT-ID>', json.dumps({
        "score": 133,
        "totalLikes": 77,
        "cheatMode": True
      }), {
-       "X-Parse-Application-Id": "lOnJ1j6c5OHzz7RFmlXATbEqGvKA3BuT9KXGyWdO",
-       "X-Parse-REST-API-Key": "sKio73fB6o0cAH7m0CbMaaHZk9GiAjiAQ5nzXYKL",
+       "X-Parse-Application-Id": "<APP-ID-HERE>",
+       "X-Parse-REST-API-Key": "<REST-KEY-HERE>",
        "Content-Type": "application/json"
      })
 result = json.loads(connection.getresponse().read().decode())
